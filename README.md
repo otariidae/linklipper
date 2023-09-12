28109a79b52818e81e92c9ed8aa2c91db82486e6
otariidae <otariidae@users.noreply.github.com>
2023-09-13 00:53:36 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Linklipper
+
+Chrome extension to copy the link text to your clipboard
+
+## Installation
+
+1. Run `deno task build`
+1. Select `dist` directory and [load an unpacked extension](https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked)
+
+## License
+
+MIT License
+
+dist/check.svg and dist/copy.svg are part of [Octicons](https://github.com/primer/octicons) and licensed under MIT License by GitHub Inc.
+
---
+++ popup.ts
@@ -0,0 +1,62 @@
+import browser from "https://esm.sh/webextension-polyfill@0.10.0";
+
+function generateScrapboxLinkText(url: string, title: string) {
+  return `[${url} ${title}]`;
+}
+
+function generateMarkdownLinkText(url: string, title: string) {
+  return `[${title}](${url})`;
+}
+
+async function onCopyButtonClick(e: Event) {
+  const target = e.target as HTMLElement;
+  if (target === null) {
+    return;
+  }
+  const container = target.closest(".link-text-container");
+  if (container === null) {
+    return;
+  }
+  const code = container.getElementsByTagName("code")[0];
+  if (code === undefined) {
+    return;
+  }
+  const codeText = code.textContent;
+  if (codeText === null) {
+    return;
+  }
+  await navigator.clipboard.writeText(codeText);
+  container.classList.add("copied");
+}
+
+async function main() {
+  const btns = document.getElementsByClassName("copy-to-clipboard");
+  for (const btn of Array.from(btns)) {
+    btn.addEventListener("click", onCopyButtonClick);
+  }
+
+  const scrapboxCode = document.getElementById("scrapbox-code");
+  const markdownCode = document.getElementById("markdown-code");
+  if (scrapboxCode === null || markdownCode === null) {
+    return;
+  }
+
+  const tabs = await browser.tabs.query({
+    active: true,
+    lastFocusedWindow: true,
+  });
+  const currentTab = tabs[0];
+  if (currentTab.url === undefined || currentTab.title === undefined) {
+    return;
+  }
+  scrapboxCode.textContent = generateScrapboxLinkText(
+    currentTab.url,
+    currentTab.title,
+  );
+  markdownCode.textContent = generateMarkdownLinkText(
+    currentTab.url,
+    currentTab.title,
+  );
+}
+
+main();
